c5944d1b5e4c47fd62d5e2c2099e3da191551cad
vikas <adhi.viki@gmail.com>
2020-06-02 22:09:28 +0530

    change password done

--- src/app/pages/change-password/change-password.page.ts
+++ src/app/pages/change-password/change-password.page.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserInterface} from '../../interfaces/user.interface';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Component({
-  selector: 'app-change-password',
-  templateUrl: './change-password.page.html',
-  styleUrls: ['./change-password.page.scss'],
+    selector: 'app-change-password',
+    templateUrl: './change-password.page.html',
+    styleUrls: ['./change-password.page.scss'],
 })
 export class ChangePasswordPage implements OnInit {
+    passwordForm: FormGroup;
+    isPasswordChanging: boolean;
+    matchPasswordError: boolean;
+    bkError = {
+        hasError: false,
+        value: ''
+    };
+    private user: UserInterface;
 
-  constructor() { }
+    constructor(private fb: FormBuilder,
+                private authenticationService: AuthenticationService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.passwordForm = this.fb.group({
+            old_password: ['', [Validators.required, Validators.maxLength(20)]],
+            new_password: ['', [Validators.required, Validators.minLength(6)]],
+            confirm_password: ['', [Validators.required, Validators.minLength(6)]],
+        });
+        this.passwordForm.controls.confirm_password.valueChanges.subscribe((value) => {
+            this.matchPasswordError = value !== this.passwordForm.controls.new_password.value;
+        });
+        this.passwordForm.controls.new_password.valueChanges.subscribe((value) => {
+            this.matchPasswordError = value !== this.passwordForm.controls.confirm_password.value;
+        });
+        this.passwordForm.valueChanges.subscribe(() => {
+            this.bkError = {
+                hasError: false,
+                value: ''
+            };
+        });
+    }
+
+    changePassword() {
+        const values = this.passwordForm.value;
+        this.matchPasswordError = values.new_password !== values.confirm_password;
+        if (this.matchPasswordError) {
+            return;
+        }
+        this.bkError = {
+            hasError: false,
+            value: ''
+        };
+        this.authenticationService.changePassword(this.user.id, values).subscribe((response) => {
+            console.log(response);
+            alert('Password changed successful.');
+        }, error => {
+            const keys = Object.keys(error.error);
+            this.bkError = {
+                hasError: true,
+                value: `${keys[0]}: ${error.error[keys[0]]}`
+            };
+        });
+    }
 
 }
